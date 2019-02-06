34b3f38cf97dafed860deb6fcf5c52e0e0180103
Amanda Kay <amandakaywiggins@gmail.com>
2019-02-06 06:48:46 -0600

    some improvements on art section

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -1,22 +1,5 @@
 $(document).ready(function() {
 
-
-
-    $("img").click(function() {
-        var id = $(event.target).attr("id");
-        console.log(id);
-
-        if (id === "slide1") {
-            window.open("https://amandakaywiggins.github.io/women-artists/index.html");
-        } else if (id === "slide2") {
-            window.open("https://amandakaywiggins.github.io/shuttle-schedule/index.html");
-        } else if (id === "slide3") {
-            window.open("https://amandakaywiggins.github.io/lit-rebels-quiz/index.html");
-        } else {
-            console.log ("nope");
-        }
-    });
-
     function launchCarousel() {
         var divID = $("#artcarousels");
         if (divID.style.display === "none") {
@@ -27,38 +10,50 @@ $(document).ready(function() {
     };
 
     $("#bodiesI").click(function() {
+        $("body").css("position", "fixed");
+        $("#artcarousels").show("slow");
         $("#artcarousels").css("display", "block");
         $(".bodiesI").css("display", "block");
         $(".heartlines").css("display", "none");
         $(".bodiesII").css("display", "none");
 
         $("#close").click(function() {
+            $("#artcarousels").scrollTop();
             $("#artcarousels").css("display", "none");
             $(".bodiesI").css("display", "none");
+            $("body").css("position", "static");
         });
     });
 
     $("#bodiesII").click(function() {
+        $("body").css("position", "fixed");
+        $("#artcarousels").show("slow");
         $("#artcarousels").css("display", "block");
         $(".bodiesII").css("display", "block");
         $(".heartlines").css("display", "none");
         $(".bodiesI").css("display", "none");
 
         $("#close").click(function() {
+            $("#artcarousels").scrollTop();
             $("#artcarousels").css("display", "none");
             $(".bodiesI").css("display", "none");
+            $("body").css("position", "static");
         });
     });
 
     $("#heartlines").click(function() {
+        $("body").css("position", "fixed");
+        $("#artcarousels").show("slow");
         $("#artcarousels").css("display", "block");
         $(".heartlines").css("display", "block");
         $(".bodiesI").css("display", "none");
         $(".bodiesII").css("display", "none");
 
         $("#close").click(function() {
+            $("#artcarousels").scrollTop();
             $("#artcarousels").css("display", "none");
             $(".heartlines").css("display", "none");
+            $("body").css("position", "static");
         });
     });
 
